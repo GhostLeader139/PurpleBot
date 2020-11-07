1d8b6e1e45117a9641e673b3dce0b2d8568bce2d
Purple Scientist <yanikyanus@gmail.com>
2020-11-07 15:33:31 +0300

    Update the bot

---
+++ cogs/help.py
@@ -0,0 +1,43 @@
+import discord
+import os
+import random
+from discord.ext import commands
+
+class Help(commands.Cog):
+
+    def _init_(self, client):
+        self.client = client
+
+    @commands.command()
+    async def help(self,ctx):
+        await ctx.send('''
+**```
+PurpleBot's commands.
+```**
+*```
+Categories:
+```*
+```
+Moderation: 
+   ban, kick, mute, unban, unmute
+```
+```
+Fun: 
+   boo, crabrave, die, hello, meme, pogchamp, predict, randnum
+```
+```
+Science: 
+   pi, ping
+```
+```
+Linux: 
+   debian, distro, groovy, interject, ubuntu
+```
+```
+Utility: 
+   about, github, help, invite, license, ping, unload
+```
+ ''')
+  
+def setup(client):
+    client.add_cog(Help(client))
